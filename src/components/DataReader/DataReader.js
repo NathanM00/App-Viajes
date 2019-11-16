1fe094b87e8aac08e35602eef83d9f452e9cc185
NathanM00 <nathanmanuelsolano@hotmail.com>
2019-11-16 16:34:49 -0500

    Pregunta 1,2 y 3 readys

--- src/components/DataReader/DataReader.js
+++ src/components/DataReader/DataReader.js
@@ -5,6 +5,7 @@ import { ListItem, List, ListItemAvatar, ListItemText, Divider, Avatar } from '@
 function DataReader() {
 
     const [rows, setRows] = React.useState([]);
+    const [rows2, setRows2] = React.useState([]);
     const [selectIndex, setSelectedIndex] = React.useState('');
     React.useEffect(() => {
         async function getData() {
@@ -19,22 +20,49 @@ function DataReader() {
         }
         getData()
     }, []) // [] means just do this once, after initial render
+    React.useEffect(() => {
+        async function getData() {
+            const response = await fetch('/resources/dataBaseDestinos.csv')
+            const reader = response.body.getReader()
+            const result = await reader.read() // raw array
+            const decoder = new TextDecoder('utf-8')
+            const csv = decoder.decode(result.value) // the csv text
+            const results = Papa.parse(csv, { header: true }) // object with { data, errors, meta }
+            const rows = results.data // array of objects
+            setRows2(rows)
+        }
+        getData()
+    }, []) // [] means just do this once, after initial render
+
 
     var newArray = Object.assign(rows);
+    var newArray2 = Object.assign(rows2);
     let objetoA;
     let objetoB;
     var nuevosK = [];
     const [listaOrdenados, setListaOrdenados] = React.useState([]);
+    const [listaDestinos2, setListaDestinos2] = React.useState([]);
+    const [listaDestinos3, setListaDestinos3] = React.useState([]);
+
     const [indexLista, setIndexLista] = React.useState('0');
+    const [indexLista2, setIndexLista2] = React.useState('0');
+    const [indexLista3, setIndexLista3] = React.useState('0');
+
     let selected = null;
     if (selectIndex !== '') selected = newArray[selectIndex];
+    let perfilGrupal = null;
 
     function handleChange(event) {
         setSelectedIndex(event.target.value);
     }
-
-    function handleChangeText(event) {
+    function handleAcompañantes(event) {
         setIndexLista(event.target.value);
+    }
+    function handleLugares2(event) {
+        setIndexLista2(event.target.value);
+    }
+    function handleLugares3(event) {
+        setIndexLista3(event.target.value);
     }
 
     function formulaCos() {
@@ -79,8 +107,7 @@ function DataReader() {
 
         let temp = listaOrdenados;
         temp.push(yoMismo);
-        console.log(temp);
-        
+
         var promedio = [];
         promedio[37] = 0;
         var ordenado;
@@ -90,7 +117,6 @@ function DataReader() {
         }
         for (let index = 0; index < temp.length; index++) {
             ordenado = temp[index].info;
-            console.log(ordenado);
             for (let y = 1; y < ordenado.length; y++) {
                 promedio[y] += parseInt(ordenado[y]);
             }
@@ -99,9 +125,75 @@ function DataReader() {
             promedio[index] = parseInt(promedio[index] / temp.length);
         }
         // Este promedio es el perfil grupal de las personas que salen en la primer pregunta sin contarte a ti mismo xd
-        console.log(promedio);
+        perfilGrupal = promedio;
+
+        let objetoC = Object.values(perfilGrupal).slice(1);
+        let objetoD;
+
+        for (let index = 0; index < newArray2.length; index++) {
+            objetoD = Object.values(newArray2[index]).slice(2);
+
+            var numerador = 0;
+            var denominadorA = 0;
+            var denominadorB = 0;
+
+            for (let index = 1; index < objetoC.length; index++) {
+                numerador += (parseInt(objetoC[index]) * parseInt(objetoD[index]));
+                denominadorA += (parseInt(objetoC[index]) * parseInt(objetoC[index]));
+                denominadorB += (parseInt(objetoD[index]) * parseInt(objetoD[index]));
+            }
+
+            denominadorA = Math.sqrt(denominadorA);
+            denominadorB = Math.sqrt(denominadorB);
+            var valorK = numerador / (denominadorA * denominadorB);
+            var valorFinalK = parseInt(valorK * 100);
+
+            if (valorFinalK < 99) {
+                nuevosK.push({
+                    destino: newArray2[index].Destino,
+                    valorK: valorFinalK + "%",
+                    info: objetoD,
+                });
+                console.log(nuevosK.destino);
+            }
+        }
+        setListaDestinos2(nuevosK.sort((a, b) => (a.valorK > b.valorK) ? - 1 : 1).slice(0, indexLista2));
 
     }
+
+    function formulaCos3() {
+        objetoA = Object.values(selected).slice(1);
+
+        for (let index = 0; index < newArray2.length; index++) {
+            objetoB = Object.values(newArray2[index]).slice(1);
+
+            var numerador = 0;
+            var denominadorA = 0;
+            var denominadorB = 0;
+
+            for (let index = 1; index < objetoA.length; index++) {
+                numerador += (parseInt(objetoA[index]) * parseInt(objetoB[index]));
+                denominadorA += (parseInt(objetoA[index]) * parseInt(objetoA[index]));
+                denominadorB += (parseInt(objetoB[index]) * parseInt(objetoB[index]));
+            }
+
+            denominadorA = Math.sqrt(denominadorA);
+            denominadorB = Math.sqrt(denominadorB);
+            var valorK = numerador / (denominadorA * denominadorB);
+            var valorFinalK = parseInt(valorK * 100);
+
+            if (valorFinalK < 99) {
+                nuevosK.push({
+                    destino: newArray2[index].Destino,
+                    valorK: valorFinalK + "%",
+                    info: objetoB,
+                });
+            }
+
+        }
+        setListaDestinos3(nuevosK.sort((a, b) => (a.valorK > b.valorK) ? - 1 : 1).slice(0, indexLista3));
+    }
+
 
     return (
         <div>
@@ -120,7 +212,7 @@ function DataReader() {
 
                 </div>}
 
-                <input onChange={handleChangeText} type='number' />
+                <input onChange={handleAcompañantes} type='number' placeholder='Numero de acompañantes'  />
                 <button onClick={formulaCos}>Start</button>
 
                 <ul>
@@ -132,7 +224,24 @@ function DataReader() {
 
             <div>
                 <h1>Pregunta 2</h1>
+                <input onChange={handleLugares2} type='number'  placeholder='Numero de lugares recomendados' />
                 <button onClick={formulaCos2}>Start</button>
+                <ul>
+                    {listaDestinos2.map((item, i) =>
+                        <li>{item.destino}{item.valorK}</li>
+                    )}
+                </ul>
+            </div>
+
+            <div>
+                <h1>Pregunta 3</h1>
+                <input onChange={handleLugares3} type='number'  placeholder='Numero de lugares recomendados' />
+                <button onClick={formulaCos3}>Start</button>
+                <ul>
+                    {listaDestinos3.map((item, i) =>
+                        <li>{item.destino}{item.valorK}</li>
+                    )}
+                </ul>
             </div>
 
         </div>
